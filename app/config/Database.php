5794c9addf0cb3b767fc62b6805d60d1a1f09ad7
Hamza Saaf <hamzasaaf725@gmail.com>
2025-01-15 11:59:39 +0100

    creat Databse BDO

---
+++ app/config/Database.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Config;
+
+use Dotenv\Dotenv;
+use PDO;
+use PDOException;
+use Exception;
+
+class DatabaseCon
+{
+    private $conn;
+
+    public function connection()
+    {
+        // Load environment variables
+        $dotenv = Dotenv::createImmutable(__DIR__);
+        try {
+            $dotenv->load();
+            echo 'connect';
+        } catch (Exception $e) {
+            die("Error loading .env file: " . $e->getMessage());
+        }
+
+        // Establish PDO connection
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $_ENV["LOCALHOST"] . ";dbname=" . $_ENV["DATABASE"],
+                $_ENV["USER"],
+                $_ENV["USER_PASSWORD"],
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Enable exceptions for errors
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC // Default fetch mode
+                ]
+            );
+            return $this->conn;
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
+}
